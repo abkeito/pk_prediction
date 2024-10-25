3cf12c751958a23242b1cf311842a29fc4c8299c
tsubo-haru1998 <tsubo-haru1998@g.ecc.u-tokyo.ac.jp>
2024-10-25 15:28:41 +0900

    add memo

--- src/prediction/model.py
+++ src/prediction/model.py
@@ -22,43 +22,52 @@ class CoodinatePredictionModel(nn.Module):
         self.output_size = output_size
         self.reset_state()
 
+        # デバイスの設定
         if torch.cuda.is_available():
             self.device = torch.device('cuda')
         else:
             self.device = torch.device('cpu')
 
+    # 隠れ層をリセット
     def reset_state(self):
         self.hr = None
 
     def encode(self, inputs):
-        for step in range(inputs.size(0)):
-            input = inputs[step]
+        # フレームごとにLSTMで隠れ層の値を更新
+        for frame in range(inputs.size(0)):
+            input = inputs[frame]
             self.hr = self.W_lstm_enc(input, self.hr)
 
     def decode(self, dec_input):
+        # 1フレームのデコードを行う。LSTMで隠れ層を更新し、W_hr_y(線形変換)で予測した座標を返す
         self.hr = self.W_lstm_dec(dec_input, self.hr)
         y = self.W_hr_y(self.hr[0])
         return y
 
-    def forward(self, inputs, outputs=None):
+    def forward(self, inputs, outputs=None): # outputsがあれば予測値との平均二乗誤差を、なければ予測値を出力
         inputs = inputs.to(self.device)
         if outputs is not None:
             outputs = outputs.to(self.device)
+        
         self.encode(inputs)
 
         batch_size = inputs.size(1)
+
+        # デコードの前にセル状態をリセット
         self.hr = (self.hr[0], torch.zeros(batch_size, self.hidden_size).to(self.device))
         dec_input = torch.zeros(batch_size, self.output_size).to(self.device)
 
         if outputs is not None:
             accum_loss = 0
             for output in outputs:
+                # フレームごとにデコードし、結果を元データと比較
                 y = self.decode(dec_input)
                 loss = self.loss_fn(y, output)
                 accum_loss = accum_loss + loss
                 dec_input = y
             return accum_loss
         else:
+            # 予測座標を出力
             result = []
             for _ in range(self.output_size):
                 y = self.decode(dec_input)
